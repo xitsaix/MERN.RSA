ceb0d9fa2f2a5be5b665ceb4b66a6f3761dc7f0d
Akbar Ihsanul Ahadin <114788608+xitsaix@users.noreply.github.com>
2024-04-14 19:55:04 +0700

    Multi-Role Login Backend using Node JS, Express, and MySQL

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,47 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+import db from "./config/Database.js";
+import SequelizeStore from "connect-session-sequelize";
+import UserRoute from "./routes/UserRoute.js";
+import ProductRoute from "./routes/ProductRoute.js";
+import AuthRoute from "./routes/AuthRoute.js";
+dotenv.config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
 
-app.listen(5000, () => console.log('Server up and Running...'))
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({
+    db: db
+});
+
+// (async()=>{
+//     await db.sync();
+// })();
+
+app.use(session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+        secure: 'auto'
+    }
+}));
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+app.use(express.json());
+app.use(UserRoute);
+app.use(ProductRoute);
+app.use(AuthRoute);
+
+// store.sync();
+
+app.listen(process.env.APP_PORT, ()=> {
+    console.log('Server up and running...');
+});
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-npm i express mysql2 sequelize cors
+npm i express mysql2 sequelize argon2 cors dotenv express-session
+
+"type": "module",
